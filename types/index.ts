dc6ae1c61e10d8a7745ddcc054a6a0f18b2c615e
RiosHectorM <rios.hector.martin@gmail.com>
2024-10-03 20:40:52 -0300

    Types, enums, utils functions, json example

--- types/index.ts
+++ types/index.ts
@@ -1,45 +1,91 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
+import { Color, Genero, Talla } from '../constants/enums';
 
-export interface CarProps {
-  city_mpg: number;
-  class: string;
-  combination_mpg: number;
-  cylinders: number;
-  displacement: number;
-  drive: string;
-  fuel_type: string;
-  highway_mpg: number;
-  make: string;
-  model: string;
-  transmission: string;
-  year: number;
+// export interface CarProps {
+//   city_mpg: number;
+//   class: string;
+//   combination_mpg: number;
+//   cylinders: number;
+//   displacement: number;
+//   drive: string;
+//   fuel_type: string;
+//   highway_mpg: number;
+//   make: string;
+//   model: string;
+//   transmission: string;
+//   year: number;
+// }
+
+// export interface FilterProps {
+//   manufacturer?: string;
+//   year?: number;
+//   model?: string;
+//   limit?: number;
+//   fuel?: string;
+// }
+
+export interface PrendasProps {
+  nombre: string;
+  descripcion?: string;
+  precio?: number;
+  categoria?: string;
+  subcategoria?: string;
+  marca?: string;
+  talla?: Talla;
+  color?: Color;
+  material?: string;
+  genero?: Genero;
+  imagen?: string;
+  inventario?: number;
+  promocion?: {
+    tipo: string;
+    porcentaje: number;
+  };
+  calificacion?: number;
+  caracteristicas?: string[];
 }
 
-export interface FilterProps {
-  manufacturer?: string;
-  year?: number;
-  model?: string;
-  limit?: number;
-  fuel?: string;
+export interface FilterPrendas {
+  precio?: number;
+  categoria?: string;
+  subcategoria?: string;
+  marca?: string;
+  talla?: Talla;
+  color?: Color;
+  material?: string;
+  genero?: Genero;
+  promocion?: {
+    tipo: string;
+    porcentaje: number;
+  };
 }
 
 export interface HomeProps {
-  searchParams: FilterProps;
+  searchParams: FilterPrendas;
 }
 
-export interface CarCardProps {
-  model: string;
-  make: string;
-  mpg: number;
-  transmission: string;
-  year: number;
-  drive: string;
-  cityMPG: number;
+export interface PrendasCardProps {
+  nombre: string;
+  precio?: number;
+  marca?: string;
+  talla?: Talla;
+  color?: Color;
+  imagen?: string;
 }
+
+// export interface CarCardProps {
+//   model: string;
+//   make: string;
+//   mpg: number;
+//   transmission: string;
+//   year: number;
+//   drive: string;
+//   cityMPG: number;
+// }
 
 export interface CustomButtonProps {
   isDisabled?: boolean;
-  btnType?: "button" | "submit";
+  btnType?: 'button' | 'submit';
   containerStyles?: string;
   textStyles?: string;
   title: string;
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
-import { CarProps, FilterProps } from "@types";
+import { PrendasProps, FilterPrendas } from '@types';
+import { prendasEjemplo } from '../constants/prendas_ejemplo';
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
@@ -36,45 +37,63 @@ export const deleteSearchParams = (type: string) => {
   newSearchParams.delete(type.toLocaleLowerCase());
 
   // Construct the updated URL pathname with the deleted search parameter
-  const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+  const newPathname = `${
+    window.location.pathname
+  }?${newSearchParams.toString()}`;
 
   return newPathname;
 };
 
-export async function fetchCars(filters: FilterProps) {
-  const { manufacturer, year, model, limit, fuel } = filters;
+export async function fetchCars(filters: FilterPrendas) {
+  //Filtros para aplicar
+  const {
+    categoria,
+    color,
+    genero,
+    marca,
+    material,
+    precio,
+    promocion,
+    subcategoria,
+    talla,
+  } = filters;
 
-  // Set the required headers for the API request
-  const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  };
+  // // Set the required headers for the API request
+  // const headers: HeadersInit = {
+  //   "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+  //   "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  // };
 
-  // Set the required headers for the API request
-  const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-    {
-      headers: headers,
-    }
-  );
+  // // Set the required headers for the API request
+  // const response = await fetch(
+  //   `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+  //   {
+  //     headers: headers,
+  //   }
+  // );
 
-  // Parse the response as JSON
-  const result = await response.json();
-
-  return result;
+  // // Parse the response as JSON
+  // const result = await response.json();
+  
+  console.log(prendasEjemplo);
+  //devuelve los ejemplos de prendas
+  return prendasEjemplo;
 }
 
-export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  const url = new URL("https://cdn.imagin.studio/getimage");
-  const { make, model, year } = car;
+// export const generateCarImageUrl = (car: PrendasProps, angle?: string) => {
+//   const url = new URL('https://cdn.imagin.studio/getimage');
+//   const { make, model, year } = car;
 
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
-  url.searchParams.append('angle', `${angle}`);
+//   url.searchParams.append(
+//     'customer',
+//     process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ''
+//   );
+//   url.searchParams.append('make', make);
+//   url.searchParams.append('modelFamily', model.split(' ')[0]);
+//   url.searchParams.append('zoomType', 'fullscreen');
+//   url.searchParams.append('modelYear', `${year}`);
+//   // url.searchParams.append('zoomLevel', zoomLevel);
+//   url.searchParams.append('angle', `${angle}`);
 
-  return `${url}`;
-} 
+//   return `${url}`;
+//};
