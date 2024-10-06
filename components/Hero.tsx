24cdc06df5fe27ec23a7510211dcf6abe283d5a1
RiosHectorM <rios.hector.martin@gmail.com>
2024-10-05 23:52:18 -0300

    Add Images and Styles Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,36 +2,18 @@
 
 import Image from 'next/image';
 
-import { CustomButton } from '@components';
-
 const Hero = () => {
-  const handleScroll = () => {
-    const nextSection = document.getElementById('discover');
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
     <div className="hero">
-      <div className="flex-1 pt-28 padding-x text-slate-100">
-        <h1 className="hero__title">
-          Encontra la ropa que marca tu estilo!
-        </h1>
-
-        <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
-        />
+      <div className="flex-1 pt-14 padding-x ">
+        <h1 className="hero__title">Tienda de Ropa Deportiva y Urbana</h1>
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/hero2.png" alt="hero" fill className="object-contain" />
         </div>
 
-        <div className="hero__image-overlay" />
+        <div className="hero__image-overlay " />
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,21 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import CustomButton from './CustomButton';
-
 const NavBar = () => (
   <header className="w-full  absolute z-10">
     <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
       <Link href="/" className="flex justify-center items-center">
         <Image
-          src="/logo.svg"
+          src="/logo.png"
           alt="logo"
-          width={118}
+          width={80}
           height={18}
-          className="object-contain"
+          className="object-contain bg-white rounded-full "
         />
       </Link>
-
-      {/* <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-      /> */}
+      <div className="flex-1 padding-x  text-2xl font-extrabold">
+        <h1 className="">Encontra la ropa que marca tu estilo!</h1>
+      </div>
     </nav>
   </header>
 );
